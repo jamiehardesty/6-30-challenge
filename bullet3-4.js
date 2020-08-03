9156349a6743d2aae8019998e4e39411db672f89
jamiehardesty <64821697+jamiehardesty@users.noreply.github.com>
2020-08-02 19:20:29 -0700

    Initial Upload

---
+++ bullet3-4.js
@@ -0,0 +1,3 @@
+var steiner = Math.floor((Math.random() * 115) + 1);
+
+alert('Hello Tobias. The Web35 class contains 115 students.') + alert('I have chosen a random number between 1 and 115. It is ' + steiner + '. I rock!')
---
+++ bullet5-6.js
@@ -0,0 +1,11 @@
+function mySubmission() {
+    var txt;
+    var tobias = prompt("How many challenge submissions were correct?", "Surprise me!");
+    var steiner = Math.floor((Math.random() * tobias) + 1);
+    if (tobias == null || tobias == "") {
+      txt = "User cancelled the prompt.";
+    } else {
+      txt = "I have computed a random number between 1 and " + tobias + ". It is " + steiner + ". Alphabetically sort the names of those who submitted correct answers and select the person at the number I chose as the winner. Thank you!";
+    }
+    document.getElementById("replaceH1").innerHTML = txt;
+  }
