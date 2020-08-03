f84fb1b433fcd772bc531b51f88bcb66db026f8a
jamiehardesty <64821697+jamiehardesty@users.noreply.github.com>
2020-08-02 19:55:24 -0700

    Update!

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
-var steiner = Math.floor((Math.random() * 115) + 1);
+var steiner = Math.floor((Math.random() * 157) + 1);
 
-alert('Hello Tobias. The Web35 class contains 115 students.') + alert('I have chosen a random number between 1 and 115. It is ' + steiner + '. I rock!')
+alert('Hello Tobias. The Web35 class contains 157 students.') + alert('I have chosen a random number between 1 and 157. It is ' + steiner + '. I rock!')
 
 function mySubmission() {
     var txt;
