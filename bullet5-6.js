42ee85f2c4146aded2c4077514b3e617aedfc741
Jamie Hardesty <jamiehardesty@outlook.com>
2020-08-02 19:45:23 -0700

    Oh, I forgot how this works, whomp.

--- bullet5-6.js
+++ bullet5-6.js
@@ -1,3 +1,7 @@
+var steiner = Math.floor((Math.random() * 115) + 1);
+
+alert('Hello Tobias. The Web35 class contains 115 students.') + alert('I have chosen a random number between 1 and 115. It is ' + steiner + '. I rock!')
+
 function mySubmission() {
     var txt;
     var tobias = prompt("How many challenge submissions were correct?", "Surprise me!");
